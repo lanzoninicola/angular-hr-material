1d65e6ff7d036f06377e516a19f1ab9547f9621d
Nicola <lanzoni.nicola@gmail.com>
2022-01-03 15:07:29 -0300

    Added trashee module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TrasheeModule } from './trashee/trashee.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    TrasheeModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
