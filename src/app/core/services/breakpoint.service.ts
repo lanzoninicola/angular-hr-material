bea8c692015eab6cf7344ad9c417f92e2b837cb7
Nicola <lanzoni.nicola@gmail.com>
2022-01-11 20:55:18 -0300

    Added Breakpoint Service

---
+++ src/app/core/services/breakpoint.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map, Observable, shareReplay } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BreakpointService {
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  isHandset() {}
+}
