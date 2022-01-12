9fa40b6e5996009b21fbd568c554686790b28e12
Nicola <lanzoni.nicola@gmail.com>
2022-01-11 21:00:07 -0300

    Removed module not used

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,14 @@
 import { NgModule } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TrasheeModule } from './trashee/trashee.module';
 import { CoreModule } from './core/core.module';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
 import { SharedModule } from './shared/shared.module';
+import { TrasheeModule } from './trashee/trashee.module';
 import { UsersModule } from './users/users.module';
 
 @NgModule({
@@ -20,10 +17,7 @@ import { UsersModule } from './users/users.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
     MatSidenavModule,
-    MatIconModule,
     MatListModule,
     TrasheeModule,
     CoreModule,
