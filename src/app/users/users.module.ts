ab9ad5c6afb1f68ff688ebba4f1d9bc363724696
Nicola <lanzoni.nicola@gmail.com>
2022-01-13 20:42:36 -0300

    Added required modules for the UserEditComponent

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,24 +1,50 @@
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatOptionModule } from '@angular/material/core';
+import { MatDividerModule } from '@angular/material/divider';
+import {
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+  MatFormFieldModule,
+} from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 
 import { CoreModule } from '../core/core.module';
 import { SharedModule } from '../shared/shared.module';
+import { UserEditComponent } from './components/user-edit/user-edit.component';
 import { UserListComponent } from './components/user-list/user-list.component';
+import { UsersSectionComponent } from './components/users-section/users-section.component';
 import { UsersRoutingModule } from './users-routing.module';
 
 @NgModule({
-  declarations: [UserListComponent],
+  declarations: [UserListComponent, UserEditComponent, UsersSectionComponent],
   imports: [
     CommonModule,
     CoreModule,
     SharedModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatTableModule,
     MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatDividerModule,
+    MatOptionModule,
     UsersRoutingModule,
+  ],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'outline' },
+    },
   ],
 })
 export class UsersModule {}
