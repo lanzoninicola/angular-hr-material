027d8f6460cd852e605f4b783370202f289d3f2b
Nicola <lanzoni.nicola@gmail.com>
2022-01-14 18:32:08 -0300

    Created Table Paginator component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,14 +2,26 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { HeaderComponent } from './components/header/header.component';
 import { SectionToolbarComponent } from './components/section-toolbar/section-toolbar.component';
+import { TablePaginatorComponent } from './components/table-paginator/table-paginator.component';
 
 @NgModule({
-  declarations: [HeaderComponent, SectionToolbarComponent],
-  imports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule],
-  exports: [HeaderComponent, SectionToolbarComponent],
+  declarations: [
+    HeaderComponent,
+    SectionToolbarComponent,
+    TablePaginatorComponent,
+  ],
+  imports: [
+    CommonModule,
+    MatPaginatorModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  exports: [HeaderComponent, SectionToolbarComponent, TablePaginatorComponent],
 })
 export class SharedModule {}
