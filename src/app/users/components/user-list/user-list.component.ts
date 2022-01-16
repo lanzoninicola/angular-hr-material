4134da4b5dc165ea0ee932952e9ef52dad57eb9e
Nicola <lanzoni.nicola@gmail.com>
2022-01-16 12:08:32 -0300

    Updated the UserListComponent with the new TableDataComponent

--- src/app/users/components/user-list/user-list.component.ts
+++ src/app/users/components/user-list/user-list.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { PageEvent } from '@angular/material/paginator';
-import { ActivatedRoute, Router } from '@angular/router';
-import { BehaviorSubject, combineLatest, Subscription } from 'rxjs';
-import { TableColumn } from 'src/app/shared/types/table.types';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 import { UsersService } from '../../services/users.service';
 import { UserModel } from '../../types/user.type';
@@ -12,93 +10,34 @@ import { UserModel } from '../../types/user.type';
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss'],
 })
-export class UserListComponent implements OnDestroy {
-  private _usersSubscription: Subscription;
-  // tableDataSource$: BehaviorSubject<UserModel[]> = new BehaviorSubject<
-  //   UserModel[]
-  // >([]);
-  displayedColumns$: BehaviorSubject<string[]> = new BehaviorSubject<string[]>([
-    'lastname',
-    'firstname',
-    'email',
-    'department',
-    'companyRoleLevel',
-  ]);
-
-  columns$: BehaviorSubject<TableColumn[]> = new BehaviorSubject<TableColumn[]>(
-    [
-      {
-        title: 'Lastname',
-        dsFieldName: 'lastname',
-      },
-      {
-        title: 'Firstname',
-        dsFieldName: 'firstname',
-      },
-      {
-        title: 'E-mail',
-        dsFieldName: 'email',
-      },
-      {
-        title: 'Department',
-        dsFieldName: 'department',
-      },
-      {
-        title: 'Level',
-        dsFieldName: 'companyRoleLevel',
-      },
-    ]
-  );
-
-  dataOnPage: any[] = [];
-
-  userListTableConfig;
+export class UserListComponent {
+  userList$: Observable<UserModel[]>;
+  columns = [
+    {
+      title: 'Lastname',
+      dsFieldName: 'lastname',
+    },
+    {
+      title: 'Firstname',
+      dsFieldName: 'firstname',
+    },
+    {
+      title: 'E-mail',
+      dsFieldName: 'email',
+    },
+    {
+      title: 'Department',
+      dsFieldName: 'department',
+    },
+    {
+      title: 'Level',
+      dsFieldName: 'companyRoleLevel',
+    },
+  ];
+  pageSize = 10;
 
   constructor(private usersService: UsersService, private router: Router) {
-    // this._usersSubscription = this.usersService
-    //   .getAllUsers()
-    //   .subscribe(this.tableDataSource$);
-
-    let dataSource: any[] = [];
-    this._usersSubscription = this.usersService
-      .getAllUsers()
-      .subscribe((users) => {
-        console.log(users);
-        dataSource = [...dataSource, ...users];
-      });
-
-    console.log(dataSource);
-
-    this.userListTableConfig = {
-      dataSource: dataSource,
-      columns: [
-        {
-          title: 'Lastname',
-          dsFieldName: 'lastname',
-        },
-        {
-          title: 'Firstname',
-          dsFieldName: 'firstname',
-        },
-        {
-          title: 'E-mail',
-          dsFieldName: 'email',
-        },
-        {
-          title: 'Department',
-          dsFieldName: 'department',
-        },
-        {
-          title: 'Level',
-          dsFieldName: 'companyRoleLevel',
-        },
-      ],
-      pageSize: 5,
-    };
-  }
-
-  ngOnDestroy(): void {
-    this._usersSubscription.unsubscribe();
+    this.userList$ = this.usersService.getAllUsers();
   }
 
   handleTableRowClicked(userRow: UserModel) {
