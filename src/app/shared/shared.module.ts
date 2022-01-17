5db881de3dca08253ae22dc771edf2da7fb46c50
Nicola <lanzoni.nicola@gmail.com>
2022-01-17 20:00:28 -0300

    Added Form Builder Component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { HeaderComponent } from './components/header/header.component';
 import { SectionToolbarComponent } from './components/section-toolbar/section-toolbar.component';
-import { TablePaginatorComponent } from './components/table-paginator/table-paginator.component';
 import { TableDataComponent } from './components/table-data/table-data.component';
-import { MatTableModule } from '@angular/material/table';
+import { TablePaginatorComponent } from './components/table-paginator/table-paginator.component';
+import { FormControlComponent } from './components/form-control/form-control.component';
+import { FormComponent } from './components/form/form.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatOptionModule } from '@angular/material/core';
+import { FormGroupComponent } from './components/form-group/form-group.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +26,9 @@ import { MatTableModule } from '@angular/material/table';
     SectionToolbarComponent,
     TablePaginatorComponent,
     TableDataComponent,
+    FormComponent,
+    FormControlComponent,
+    FormGroupComponent,
   ],
   imports: [
     CommonModule,
@@ -25,12 +37,21 @@ import { MatTableModule } from '@angular/material/table';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatIconModule,
+    MatDividerModule,
+    MatOptionModule,
   ],
   exports: [
     HeaderComponent,
     SectionToolbarComponent,
     TablePaginatorComponent,
     TableDataComponent,
+    FormComponent,
   ],
 })
 export class SharedModule {}
