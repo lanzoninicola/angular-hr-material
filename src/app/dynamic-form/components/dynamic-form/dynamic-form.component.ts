691d099ee751003c528b85c417326048291635b7
Nicola <lanzoni.nicola@gmail.com>
2022-01-20 15:38:11 -0300

    Move the form building process up to the UI components layer

--- src/app/dynamic-form/components/dynamic-form/dynamic-form.component.ts
+++ src/app/dynamic-form/components/dynamic-form/dynamic-form.component.ts
@@ -1,12 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
-import { DynamicFormService } from '../../services/dynamic-form.service';
-
 @Component({
   selector: 'ahr-dynamic-form',
   template: `
-    <form [formGroup]="mainForm">
+    <form [formGroup]="formModel">
       <div *ngFor="let groupConfig of formTemplate | keyvalue">
         <ahr-dynamic-form-group [groupConfig]="groupConfig">
         </ahr-dynamic-form-group>
@@ -19,11 +17,10 @@ export class DynamicFormComponent implements OnInit {
   @Input()
   formTemplate: {};
 
-  mainForm: FormGroup;
+  @Input()
+  formModel: FormGroup;
 
-  constructor(private df: DynamicFormService) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this.mainForm = this.df.buildModel(this.formTemplate);
-  }
+  ngOnInit(): void {}
 }
