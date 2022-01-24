0440bf9f93b99bc67caba9faa689959ce3151b61
Nicola <lanzoni.nicola@gmail.com>
2022-01-24 16:21:44 -0300

    Removed unused component in Shared Module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,23 +2,15 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
 import { SectionToolbarComponent } from './components/section-toolbar/section-toolbar.component';
 import { TableDataComponent } from './components/table-data/table-data.component';
 import { TablePaginatorComponent } from './components/table-paginator/table-paginator.component';
-import { FormControlComponent } from './components/form-control/form-control.component';
-import { FormComponent } from './components/form/form.component';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatOptionModule } from '@angular/material/core';
-import { FormGroupComponent } from './components/form-group/form-group.component';
 
 @NgModule({
   declarations: [
@@ -26,32 +18,23 @@ import { FormGroupComponent } from './components/form-group/form-group.component
     SectionToolbarComponent,
     TablePaginatorComponent,
     TableDataComponent,
-    FormComponent,
-    FormControlComponent,
-    FormGroupComponent,
+    LogoComponent,
   ],
   imports: [
     CommonModule,
     MatPaginatorModule,
     MatTableModule,
     MatToolbarModule,
-    MatIconModule,
     MatButtonModule,
     FormsModule,
     ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatIconModule,
-    MatDividerModule,
-    MatOptionModule,
   ],
   exports: [
     HeaderComponent,
     SectionToolbarComponent,
     TablePaginatorComponent,
     TableDataComponent,
-    FormComponent,
+    LogoComponent,
   ],
 })
 export class SharedModule {}
