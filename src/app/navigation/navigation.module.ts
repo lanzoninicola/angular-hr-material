8e81094f6beedc1841c41d3897414a159d3f0c54
Nicola <lanzoni.nicola@gmail.com>
2022-01-24 17:31:28 -0300

    Added style to Navigation Module's components

--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatExpansionModule } from '@angular/material/expansion';
+
 import { RouterModule } from '@angular/router';
 
 import { NavListItemComponent } from './components/nav-list-item/nav-list-item.component';
@@ -9,7 +11,13 @@ import { NavListComponent } from './components/nav-list/nav-list.component';
 
 @NgModule({
   declarations: [NavListItemComponent, NavListComponent],
-  imports: [CommonModule, RouterModule, MatListModule, MatIconModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatListModule,
+    MatIconModule,
+    MatExpansionModule,
+  ],
   exports: [NavListComponent],
 })
 export class NavigationModule {}
