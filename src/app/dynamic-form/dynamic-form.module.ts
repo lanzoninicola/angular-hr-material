60b1c08596145d0c7ba9b9cbc8f30c44cc255d2b
Nicola <lanzoni.nicola@gmail.com>
2022-01-24 16:23:50 -0300

    Added provider in the Form Module

--- src/app/dynamic-form/dynamic-form.module.ts
+++ src/app/dynamic-form/dynamic-form.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatOptionModule } from '@angular/material/core';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+  MatFormFieldModule,
+} from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { DynamicFormGroupComponent } from './components/dynamic-form-group/dynamic-form-group.component';
 
+import { DynamicFormGroupComponent } from './components/dynamic-form-group/dynamic-form-group.component';
 import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
 import { FormInputComponent } from './components/form-input/form-input.component';
 import { FormSelectComponent } from './components/form-select/form-select.component';
@@ -32,6 +35,12 @@ import { DynamicFieldsDirective } from './directives/dynamic-fields.directive';
     MatIconModule,
     MatDividerModule,
     MatOptionModule,
+  ],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: { appearance: 'outline' },
+    },
   ],
   exports: [DynamicFormComponent],
 })
