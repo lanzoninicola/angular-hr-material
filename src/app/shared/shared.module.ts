6dd95c6aba5f0c5520cfe76e54b55fe22ba95c63
Nicola <lanzoni.nicola@gmail.com>
2022-01-25 16:29:48 -0300

    Removed old paginator

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,25 +2,23 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { HeaderComponent } from './components/header/header.component';
 import { LogoComponent } from './components/logo/logo.component';
+import { SearchControlComponent } from './components/search-control/search-control.component';
 import { SectionToolbarComponent } from './components/section-toolbar/section-toolbar.component';
 import { TableDataComponent } from './components/table-data/table-data.component';
-import { TablePaginatorComponent } from './components/table-paginator/table-paginator.component';
-import { SearchControlComponent } from './components/search-control/search-control.component';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     SectionToolbarComponent,
-    TablePaginatorComponent,
     TableDataComponent,
     LogoComponent,
     SearchControlComponent,
@@ -40,7 +38,6 @@ import { MatIconModule } from '@angular/material/icon';
   exports: [
     HeaderComponent,
     SectionToolbarComponent,
-    TablePaginatorComponent,
     TableDataComponent,
     LogoComponent,
   ],
