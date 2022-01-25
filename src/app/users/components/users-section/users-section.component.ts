821e8cdaab7af516991b24ebded4462862b2b14c
Nicola <lanzoni.nicola@gmail.com>
2022-01-25 18:20:53 -0300

    Working on creation of new user

--- src/app/users/components/users-section/users-section.component.ts
+++ src/app/users/components/users-section/users-section.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Route, Router } from '@angular/router';
+import { Router } from '@angular/router';
+
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'ahr-users-section',
@@ -8,7 +10,11 @@ import { Route, Router } from '@angular/router';
 })
 export class UsersSectionComponent implements OnInit {
   readonly pageTitle: string = 'Users';
-  constructor(private router: Router) {}
+  constructor(private router: Router, private _usersService: UsersService) {}
+
+  addNewUser() {
+    console.log('yeaaaa new user created');
+  }
 
   ngOnInit(): void {
     this.router.navigate(['users', 'list']);
