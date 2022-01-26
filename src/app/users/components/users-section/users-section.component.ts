74f8dea692ffd058e55f8f58bfd76f61d5e9b4f3
Nicola <lanzoni.nicola@gmail.com>
2022-01-26 16:23:26 -0300

    Move the html into the component

--- src/app/users/components/users-section/users-section.component.ts
+++ src/app/users/components/users-section/users-section.component.ts
@@ -1,22 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { UsersService } from '../../services/users.service';
-
 @Component({
   selector: 'ahr-users-section',
-  templateUrl: './users-section.component.html',
+  template: `
+    <div class="container-section">
+      <app-section-toolbar [title]="pageTitle">
+        <ahr-search-control></ahr-search-control>
+        <button mat-flat-button color="primary" (click)="addNewUser()">
+          Add New User
+        </button>
+      </app-section-toolbar>
+      <router-outlet></router-outlet>
+    </div>
+  `,
   styleUrls: ['./users-section.component.scss'],
 })
 export class UsersSectionComponent implements OnInit {
-  readonly pageTitle: string = 'Users';
-  constructor(private router: Router, private _usersService: UsersService) {}
+  pageTitle: string = 'Users';
+  constructor(private router: Router) {}
 
   addNewUser() {
-    console.log('yeaaaa new user created');
+    this.router.navigate(['users', 'new']);
+    this.pageTitle = 'New User';
   }
 
   ngOnInit(): void {
     this.router.navigate(['users', 'list']);
+    this.pageTitle = 'Users';
   }
 }
