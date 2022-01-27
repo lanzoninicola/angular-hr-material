6252cf2197561e395bc9a406bcbae974d8c0418f
Nicola <lanzoni.nicola@gmail.com>
2022-01-27 11:58:35 -0300

    Added type for the UserFormData

--- src/app/users/types/user-edit-form.types.ts
+++ src/app/users/types/user-edit-form.types.ts
@@ -3,3 +3,12 @@ export interface UserEditFormPicklist {
   departmentsValues?: string[];
   platformRoleValues?: string[];
 }
+
+export interface UserFormData {
+  firstname: string;
+  lastname: string;
+  email: string;
+  departments: string;
+  companyLevels: string;
+  platformRoles: string;
+}
