9131ad5155948242b562a71a6b70b6eba36bbb30
Nicola <lanzoni.nicola@gmail.com>
2022-01-29 21:44:26 -0300

    Refactor of ModuleStoreService

--- src/app/core/services/module-store.service.ts
+++ src/app/core/services/module-store.service.ts
@@ -6,37 +6,81 @@ import { Store, StoreService } from 'src/app/core/types/store.types';
   providedIn: 'root',
 })
 export abstract class ModuleStoreService implements StoreService {
-  store$: BehaviorSubject<Store> = new BehaviorSubject<Store>({});
-  storeValue$: BehaviorSubject<any> = new BehaviorSubject<any>(null);
+  private _store: Store = {};
+  private _newStore: Store = {};
 
   constructor() {}
 
-  get value() {
-    return this.store$.value;
-  }
+  /**
+   * @description Responsible to dispatch the new Store
+   * You might subscribe to it if necessary
+   */
+  readonly dispatcher: BehaviorSubject<any> = new BehaviorSubject<any>({});
 
+  /**
+   * @description Returns the value from the store
+   * @param key - The key that identifiers the store value
+   * @param value - The value to store
+   */
   set(key: string, value: any): void {
-    const sub = combineLatest([this.store$, of({ [key]: value })]).subscribe(
-      ([currentStore, newState]) => {
-        currentStore[key] = newState[key];
-      }
-    );
+    this._newStore = this._cloneStore();
 
-    sub.unsubscribe();
+    this._newStore[key] = value;
+
+    this._mergeStores();
+
+    this.dispatch(this._newStore);
+
+    this._clean(this._newStore);
   }
 
-  get(key: string): BehaviorSubject<any> {
-    const _storeValue$ = this.store$.pipe(
-      map((currentStore) => {
-        if (currentStore.hasOwnProperty(key)) {
-          return currentStore[key];
-        }
-      })
-    );
+  /**
+   * @description Subscribes to dispatcher and returns the value from the store
+   * @param key | optional. If not return the store
+   */
+  get(key?: string): any | null {
+    if (!key) {
+      return this._store;
+    }
 
-    const sub = _storeValue$.subscribe((data) => this.storeValue$.next(data));
+    let _storeData;
+
+    const sub = this.dispatcher.subscribe((storeData: Store) => {
+      if (storeData.hasOwnProperty(key)) {
+        _storeData = storeData[key];
+      }
+    });
+
     sub.unsubscribe();
+    return _storeData;
+  }
 
-    return this.storeValue$;
+  /**
+   * @description Returns the value from the store
+   * @param key | optional
+   */
+  dispatch(newStore: Store) {
+    this.dispatcher.next(newStore);
+  }
+
+  /**
+   * @description Make a copy of store
+   */
+  private _cloneStore() {
+    return { ...this._store };
+  }
+
+  /**
+   * @description Merge the current store with the new store
+   */
+  private _mergeStores() {
+    this._store = { ...this._store, ...this._newStore };
+  }
+
+  /**
+   * @description Reset the store given
+   */
+  private _clean(store: Store) {
+    store = {};
   }
 }
