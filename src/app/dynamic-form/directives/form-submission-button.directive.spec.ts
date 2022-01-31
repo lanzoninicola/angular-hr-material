1a8f0f6a0242cbf8675979cecfd564fabbc101a1
Nicola <lanzoni.nicola@gmail.com>
2022-01-31 16:05:35 -0300

    Remove directive, replaced with the interceptor

--- src/app/dynamic-form/directives/form-submission-button.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FormSubmissionButtonDirective } from './form-submission-button.directive';
-
-describe('FormSubmissionButtonDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FormSubmissionButtonDirective();
-    expect(directive).toBeTruthy();
-  });
-});
