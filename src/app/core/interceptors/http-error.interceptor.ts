fc7f8567c8ad1f96afe0f3701c7fe19143d9b5d1
Nicola <lanzoni.nicola@gmail.com>
2022-01-31 18:21:14 -0300

    Created HTTPErrorInterceptor

---
+++ src/app/core/interceptors/http-error.interceptor.ts
@@ -0,0 +1,80 @@
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { DynamicFormService } from 'src/app/dynamic-form/services/dynamic-form.service';
+import { MessageService } from '../services/message.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HttpErrorInterceptor implements HttpInterceptor {
+  constructor(
+    private _messageService: MessageService,
+    private _dynamicForm: DynamicFormService
+  ) {}
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next
+      .handle(request)
+
+      .pipe(
+        retry(1),
+
+        catchError((error: HttpErrorResponse) => {
+          let errorMessage = '';
+
+          if (error.error instanceof ErrorEvent) {
+            // client-side code fail to generate the request and throw the error (ErrorEvent objects)
+            errorMessage = `Error: ${error.error.message}`;
+          } else {
+            // the server might reject the request for various reasons
+            errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+          }
+
+          this._resetFormState();
+
+          this._logToUser(errorMessage);
+
+          this._logToService(errorMessage);
+
+          return throwError(() => errorMessage);
+        })
+      );
+  }
+
+  // TODO: How this works when...? See below
+
+  private _resetFormState() {
+    this._dynamicForm.idle();
+  }
+
+  private _logToUser(message: string) {
+    this._messageService.send(message, 'error');
+  }
+
+  private _logToService(message: string) {}
+}
+
+/**
+ * Use case
+ *
+ * Open the user edit form (or whatever form in the app)
+ * Start to editing the tab but not save
+ * Open a new browser tab with different form
+ * The last tab has an http specific error
+ *
+ * Go back to the first tab, continue editing and save
+ * what happens?
+ *
+ *
+ */
