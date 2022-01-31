bab13edf6ee8f4520860481a2e2f76986226e017
Nicola <lanzoni.nicola@gmail.com>
2022-01-31 18:22:09 -0300

    Handle sucessfully response of form submission

--- src/app/dynamic-form/interceptors/form-submission.interceptor.ts
+++ src/app/dynamic-form/interceptors/form-submission.interceptor.ts
@@ -8,12 +8,16 @@ import {
 } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { EMPTY, Observable, tap } from 'rxjs';
+import { MessageService } from 'src/app/core/services/message.service';
 import { DynamicFormService } from '../services/dynamic-form.service';
 import { FormState } from '../types/form-state.types';
 
 @Injectable({ providedIn: 'root' })
 export class FormSubmissionInterceptor implements HttpInterceptor {
-  constructor(private _dynamicForm: DynamicFormService) {}
+  constructor(
+    private _dynamicForm: DynamicFormService,
+    private _messageService: MessageService
+  ) {}
 
   intercept(
     req: HttpRequest<any>,
@@ -30,6 +34,13 @@ export class FormSubmissionInterceptor implements HttpInterceptor {
           tap((event) => {
             if (event instanceof HttpResponse) {
               this._dynamicForm.idle();
+            }
+          }),
+          tap((event) => {
+            if (event instanceof HttpResponse) {
+              if (event.ok) {
+                this._messageService.send('Record saved correctly', 'info');
+              }
             }
           })
         );
