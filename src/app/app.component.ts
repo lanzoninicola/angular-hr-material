375fd223eb1e9a7ef2b4a109c897e9cc862bf54e
Nicola <lanzoni.nicola@gmail.com>
2022-02-01 16:35:53 -0300

    Added Global Loading Spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
-import { Observable } from 'rxjs';
+import { ResolveEnd, ResolveStart, Router, RouterEvent } from '@angular/router';
+import { filter, Observable, Subscription } from 'rxjs';
 import { BreakpointService } from './core/services/breakpoint.service';
 
 @Component({
@@ -8,14 +9,44 @@ import { BreakpointService } from './core/services/breakpoint.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   @ViewChild('drawer') drawer: MatSidenav | any = null;
-
+  loading: boolean = false;
   isHandset$: Observable<boolean>;
 
-  constructor(private breakpointService: BreakpointService) {
+  subs: Subscription = new Subscription();
+
+  constructor(
+    private breakpointService: BreakpointService,
+    private router: Router
+  ) {
     this.isHandset$ = this.breakpointService.isHandset$;
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this._listenForLoadingSpinner();
+  }
+
+  private _listenForLoadingSpinner() {
+    this.subs.add(
+      this.router.events
+        .pipe(
+          filter(
+            (event) =>
+              event instanceof ResolveStart || event instanceof ResolveEnd
+          )
+        )
+        .subscribe((event) => {
+          if (event instanceof ResolveStart) {
+            this.loading = true;
+          } else if (event instanceof ResolveEnd) {
+            this.loading = false;
+          }
+        })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
+  }
 }
