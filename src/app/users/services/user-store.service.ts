06b7bcfd029d86c2de9b8f6bea2877b06fa464d2
Nicola <lanzoni.nicola@gmail.com>
2022-02-01 20:20:06 -0300

    Added method to the Users Store Module

--- src/app/users/services/user-store.service.ts
+++ src/app/users/services/user-store.service.ts
@@ -1,7 +1,24 @@
 import { Injectable } from '@angular/core';
 import { ModuleStoreService } from 'src/app/core/services/module-store.service';
+import { UserModel } from '../types/user.type';
 
 @Injectable({
   providedIn: 'root',
 })
-export class UsersStoreService extends ModuleStoreService {}
+export class UsersStoreService extends ModuleStoreService {
+  entityStateUpdate() {
+    this.set('userEdit-entityState', 'update');
+  }
+
+  entityStateCreate() {
+    this.set('userEdit-entityState', 'create');
+  }
+
+  set currentEntity(currentUser: UserModel) {
+    this.set('userEdit-currentUser', currentUser);
+  }
+
+  get currentEntity() {
+    return this.get('userEdit-currentUser');
+  }
+}
