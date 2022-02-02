cd7c21f2047e9a411f053128b46847d3169150b6
Nicola <lanzoni.nicola@gmail.com>
2022-02-02 10:50:06 -0300

    Created template component for checkbox

---
+++ src/app/dynamic-form/components/form-checkbox/form-checkbox.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormGroup } from '@angular/forms';
+
+import { DynamicFormService } from '../../services/dynamic-form.service';
+import { InputCheckboxConfig } from '../../types/form-control.types';
+
+@Component({
+  selector: 'ahr-form-checkbox',
+  template: ` <div [formGroup]="parentFormGroupModel">
+    <mat-checkbox
+      [labelPosition]="labelPosition"
+      [formControlName]="controlConfig['key']"
+    >
+      {{ controlConfig['label'] }}
+    </mat-checkbox>
+  </div>`,
+  styleUrls: ['./form-checkbox.component.scss'],
+})
+export class FormCheckboxComponent implements OnInit {
+  checked = false;
+  labelPosition: 'before' | 'after' = 'after';
+
+  // START: Inputs from directive: DynamicFields
+  controlConfig: InputCheckboxConfig;
+  parentGroupName: string;
+  // END: Inputs from directive: DynamicFields
+
+  control: AbstractControl;
+  parentFormGroupModel: FormGroup;
+
+  constructor(private _dynamicForm: DynamicFormService) {}
+
+  ngOnInit(): void {
+    this.parentFormGroupModel = this._dynamicForm.getFormGroup(
+      this.parentGroupName
+    );
+
+    this.control =
+      this.parentFormGroupModel.controls[this.controlConfig['key']];
+
+    console.log(this.control.value);
+  }
+}
