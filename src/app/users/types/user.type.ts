1a70ad043d87495898dca3125ac94e60113e8a7e
Nicola <lanzoni.nicola@gmail.com>
2022-02-04 18:23:39 -0300

    Renamed interface

---
+++ src/app/users/types/user.type.ts
@@ -0,0 +1,10 @@
+export interface UserDTO {
+  id: number;
+  firstname: string;
+  lastname: string;
+  email: string;
+  recruitingRole: string;
+  department: string;
+  companyRoleLevel: string;
+  isAdmin: boolean;
+}
