5aea5279b9ca00079af9a4744f5afb9daf53163e
Nicola <lanzoni.nicola@gmail.com>
2022-02-04 09:31:14 -0300

    Created Branch model

---
+++ src/app/core/models/branch.model.ts
@@ -0,0 +1,43 @@
+export class BranchModel {
+  id: number;
+  name: string;
+  street: string;
+  city: string;
+  country: string;
+  timezone: string;
+
+  constructor(
+    id: number,
+    name: string,
+    street: string,
+    city: string,
+    country: string,
+    timezone: string
+  ) {
+    this.id = id;
+    this.name = name;
+    this.street = street;
+    this.city = city;
+    this.country = country;
+    this.timezone = timezone;
+  }
+
+  getId(): number {
+    return this.id;
+  }
+  getName(): string {
+    return this.name;
+  }
+  getStreet(): string {
+    return this.street;
+  }
+  getCity(): string {
+    return this.city;
+  }
+  getCountry(): string {
+    return this.country;
+  }
+  getTimezone(): string {
+    return this.timezone;
+  }
+}
