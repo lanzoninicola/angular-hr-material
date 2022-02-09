67b73e7f357de5f1c34ceb0530a05e479811cd95
Nicola <lanzoni.nicola@gmail.com>
2022-02-09 13:50:14 -0300

    Create IconComponent

---
+++ src/app/shared/components/icon/icon.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-icon',
+  template: ` <mat-icon
+    aria-hidden="false"
+    attr.aria-label="{{ label }}"
+    [ngStyle]="{ color: color }"
+    >{{ icon }}</mat-icon
+  >`,
+  styleUrls: ['./icon.component.scss'],
+})
+export class IconComponent implements OnInit {
+  @Input()
+  icon: string;
+
+  @Input()
+  label: string;
+
+  @Input()
+  color: string;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
