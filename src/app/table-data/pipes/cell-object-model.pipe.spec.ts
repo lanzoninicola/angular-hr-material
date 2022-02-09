1131e751ee59650f7cd880d3c80c82192283ca26
Nicola <lanzoni.nicola@gmail.com>
2022-02-09 11:51:07 -0300

    Remove unused component

--- src/app/table-data/pipes/cell-object-model.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CellObjectModelPipe } from './cell-object-model.pipe';
-
-describe('CellObjectModelPipe', () => {
-  it('create an instance', () => {
-    const pipe = new CellObjectModelPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
