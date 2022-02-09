28947044614269cfe5c69a17279cc5723fe691e7
Nicola <lanzoni.nicola@gmail.com>
2022-02-09 13:52:48 -0300

    Export stringify and componentOutlet pipe

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,9 +2,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { StringifyPipe } from './pipes/stringify.pipe';
+import { ComponentOutletDirective } from './directives/component-outlet.directive';
 
 @NgModule({
-  declarations: [],
+  declarations: [StringifyPipe, ComponentOutletDirective],
   imports: [CommonModule, MatToolbarModule, MatSnackBarModule],
+  exports: [StringifyPipe, ComponentOutletDirective],
 })
 export class CoreModule {}
