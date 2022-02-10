3488e3c56506fc2da61ade1ec6e5802922ab2103
Nicola <lanzoni.nicola@gmail.com>
2022-02-10 20:50:45 -0300

    Created Picklist model

--- src/app/core/types/picklist.type.ts
+++ src/app/core/types/picklist.type.ts
@@ -1,7 +1,32 @@
-export interface Picklist {
+export class PicklistModel {
   id: number;
+  value: PicklistValue;
   type: string;
-  values: PicklistValue[];
+
+  constructor(id: number, value: PicklistValue, type: string) {
+    this.id = id;
+    this.value = value;
+    this.type = type;
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  getValue() {
+    return this.value;
+  }
+
+  getType() {
+    return this.type;
+  }
+}
+
+export interface Picklist {
+  id: PicklistId;
+  type: string;
+  value: PicklistValue;
 }
 
 export type PicklistValue = string;
+export type PicklistId = number;
