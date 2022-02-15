cdd4bb37612cc352a4235959428731830f0e4f66
Nicola <lanzoni.nicola@gmail.com>
2022-02-15 12:39:21 -0300

    Add ability to query picklist API with multiple types

--- src/app/settings/services/picklist/picklist-http.service.ts
+++ src/app/settings/services/picklist/picklist-http.service.ts
@@ -9,14 +9,23 @@ import { PicklistItemDTO } from '../../types/picklist-item.type';
   providedIn: 'root',
 })
 export class PicklistHttpService {
+  baseURL = `${environment.API}/picklist`;
+
   constructor(
     private http: HttpClient,
     private _httpOptions: HttpRequestOptionsService
   ) {}
 
+  findById(id: number): Observable<PicklistItemDTO> {
+    return this.http.get<PicklistItemDTO>(
+      `${this.baseURL}/${id}`,
+      this._httpOptions.isBackendRequest()
+    );
+  }
+
   findByType(type: string): Observable<PicklistItemDTO[]> {
     return this.http.get<PicklistItemDTO[]>(
-      `${environment.API}/picklist?type=${type}`,
+      `${this.baseURL}?type=${type}`,
       this._httpOptions.isBackendRequest()
     );
   }
@@ -26,7 +35,14 @@ export class PicklistHttpService {
     value: string
   ): Observable<PicklistItemDTO[]> {
     return this.http.get<PicklistItemDTO[]>(
-      `${environment.API}/picklist?type=${type}&value=${value}`,
+      `${this.baseURL}?type=${type}&value=${value}`,
+      this._httpOptions.isBackendRequest()
+    );
+  }
+
+  findByQuery(query: string): Observable<PicklistItemDTO[]> {
+    return this.http.get<PicklistItemDTO[]>(
+      `${this.baseURL}?${query}`,
       this._httpOptions.isBackendRequest()
     );
   }
