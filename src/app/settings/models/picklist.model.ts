2eb5cec8ba267f47a495411ea7296a551c66e005
Nicola <lanzoni.nicola@gmail.com>
2022-02-15 12:41:09 -0300

    Added the ability to query the PicklistModel by ID

--- src/app/settings/models/picklist.model.ts
+++ src/app/settings/models/picklist.model.ts
@@ -1,15 +1,22 @@
 import { PicklistItemModel } from './picklist-item.model';
 
 export class PicklistModel {
+  EMPTY: PicklistItemModel = new PicklistItemModel(0, '', '');
   items: PicklistItemModel[];
 
   constructor(picklistItems: PicklistItemModel[]) {
     this.items = picklistItems;
   }
 
-  findItemByValue(value: string): PicklistItemModel | undefined {
-    return this.items.find(
-      (item) => item.getValue().toLowerCase() === value.toLowerCase()
+  findItemById(id: number): PicklistItemModel {
+    return this.items.find((item) => item.getId() === id) || this.EMPTY;
+  }
+
+  findItemByValue(value: string): PicklistItemModel {
+    return (
+      this.items.find(
+        (item) => item.getValue().toLowerCase() === value.toLowerCase()
+      ) || this.EMPTY
     );
   }
 }
