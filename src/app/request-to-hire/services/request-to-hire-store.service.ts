e2c19ead7e6ecfe70b1138aa3274d06657fe105c
Nicola <lanzoni.nicola@gmail.com>
2022-02-16 15:53:21 -0300

    Removed unused props

--- src/app/request-to-hire/services/request-to-hire-store.service.ts
+++ src/app/request-to-hire/services/request-to-hire-store.service.ts
@@ -1,19 +1,14 @@
 import { Injectable } from '@angular/core';
 import { ModuleStoreService } from 'src/app/core/services/module-store.service';
 import { EntityState } from 'src/app/core/types/entityState.type';
-import { SelectOptionConfig } from 'src/app/dynamic-form/types/form-control.types';
-import { BranchModel } from 'src/app/settings/models/branch.model';
-import { DepartmentModel } from 'src/app/settings/models/department.model';
+
 import { RequestToHireModel } from '../models/request-to-hire.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RequestToHireStoreService extends ModuleStoreService {
-  readonly PREFIX: string = 'RTH_';
-  readonly DEPARTMENTS_LIST_FORM_CONTROL: string = `${this.PREFIX}_DEPARTMENTS_LIST_FORM_CONTROL`;
-  readonly BRANCHES_LIST_FORM_CONTROL: string = `${this.PREFIX}_BRANCHES_LIST_FORM_CONTROL`;
-  readonly JOB_ROLES_LIST_FORM_CONTROL: string = `${this.PREFIX}_JOB_ROLES_LIST_FORM_CONTROL`;
+  readonly PREFIX: string = 'RTH';
 
   set entities(entities: RequestToHireModel[]) {
     this.set(`${this.PREFIX}_LIST`, entities);
