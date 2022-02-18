4519ed24b6fc5bbb0b36314a078f63375263c60f
Nicola <lanzoni.nicola@gmail.com>
2022-02-18 17:51:15 -0300

    Added type to HttpService interface

--- src/app/core/types/http-service.type.ts
+++ src/app/core/types/http-service.type.ts
@@ -1,13 +1,21 @@
 import { Observable } from 'rxjs';
+import { CandidateDTO } from 'src/app/candidates/types/candidate.dto.type';
+import { JobIdDTO } from 'src/app/job-board/types/jobid.dto.type';
 import { RequestToHireDTO } from 'src/app/request-to-hire/types/request-to-hire.dto.type';
 import { BranchDTO } from 'src/app/settings/models/branch.model';
 import { BoardTemplateDTO } from 'src/app/settings/types/board-template.types';
 import { DepartmentDTO } from 'src/app/settings/types/department.type';
 import { JobRoleDTO } from 'src/app/settings/types/job-role.type';
 import { UserDTO } from 'src/app/users/types/user.type';
+import { Type } from 'typescript';
+
+export interface HttpRequestOptionParams {
+  withRelations?: boolean;
+  relations?: string[];
+}
 
 export interface HttpService {
-  findAll(): Observable<DTO>;
+  findAll(options?: HttpRequestOptionParams | undefined): Observable<DTO>;
   findById(id: number): Observable<DTO>;
   save(dto: any): Observable<DTO>;
   update(dto: any): Observable<DTO>;
@@ -25,4 +33,8 @@ type DTO =
   | DepartmentDTO
   | DepartmentDTO[]
   | JobRoleDTO
-  | JobRoleDTO[];
+  | JobRoleDTO[]
+  | JobIdDTO
+  | JobIdDTO[]
+  | CandidateDTO
+  | CandidateDTO[];
