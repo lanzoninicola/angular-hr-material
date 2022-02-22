3a241dc56e0c206189f8e6be7d6a64b003dbab64
Nicola <lanzoni.nicola@gmail.com>
2022-02-22 11:32:46 -0300

    Renaming component template

--- src/app/job-board/components/jobid-edit/jobid-applications/jobid-applications.component.ts
+++ src/app/job-board/components/jobid-edit/jobid-applications/jobid-applications.component.ts
@@ -2,15 +2,15 @@ import { Component, Input, OnInit } from '@angular/core';
 import { map, Observable } from 'rxjs';
 import { JobsApplicationsCollection } from 'src/app/job-board/models/job-application.collection';
 import { JobApplicationModel } from 'src/app/job-board/models/job-application.model';
-import { JobIdModel } from 'src/app/job-board/models/job-id.model';
+import { JobIdModel } from 'src/app/job-board/models/jobid.model';
 import { JobApplicationsService } from 'src/app/job-board/services/job-applications.service';
 
 @Component({
   selector: 'ahr-jobid-applications',
   template: `
-    <ahr-job-applications-list-table
+    <ahr-job-application-list-table
       [tableDataSource$]="tableDataSource$"
-    ></ahr-job-applications-list-table>
+    ></ahr-job-application-list-table>
   `,
 })
 export class JobidApplicationsComponent implements OnInit {
